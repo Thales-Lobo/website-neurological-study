5204e28a0303b97de915c001c8afc3e0e7f478fa
Thales <thalesloboz@gmail.com>
2024-05-18 20:13:36 -0300

    changes on global header

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import './App.css';
 import Dashboard from './components/DashBoard';
+import logo from './assets/logo.png';
 
 function App() {
   return (
     <div>
-      <h1>NeuroData</h1>
+      <div className="header">
+        <img src={logo} alt="NeuroData Logo" className="logo" />
+        <h1 className="title">Dynamic Neurological Dashboard</h1>
+      </div>
       <Dashboard />
     </div>
   );
